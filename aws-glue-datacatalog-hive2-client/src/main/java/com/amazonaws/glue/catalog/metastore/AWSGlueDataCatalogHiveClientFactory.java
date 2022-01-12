72f0016978cf7615f46421b66246dfbe73cf8255
Jongbeom Kim <kajebiii@gmail.com>
2022-01-12 16:03:04 +0900

    Delete HiveMetastoreClientFactory

--- aws-glue-datacatalog-hive2-client/src/main/java/com/amazonaws/glue/catalog/metastore/AWSGlueDataCatalogHiveClientFactory.java
+++ aws-glue-datacatalog-hive2-client/src/main/java/com/amazonaws/glue/catalog/metastore/AWSGlueDataCatalogHiveClientFactory.java
@@ -4,13 +4,13 @@ import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaHookLoader;
 import org.apache.hadoop.hive.metastore.IMetaStoreClient;
 import org.apache.hadoop.hive.metastore.api.MetaException;
-import org.apache.hadoop.hive.ql.metadata.HiveMetaStoreClientFactory;
+//import org.apache.hadoop.hive.ql.metadata.HiveMetaStoreClientFactory;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveMetastoreClientFactory;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class AWSGlueDataCatalogHiveClientFactory implements HiveMetaStoreClientFactory {
+public class AWSGlueDataCatalogHiveClientFactory {
 
-  @Override
   public IMetaStoreClient createMetaStoreClient(
       HiveConf conf, HiveMetaHookLoader hookLoader,
       boolean allowEmbedded,
